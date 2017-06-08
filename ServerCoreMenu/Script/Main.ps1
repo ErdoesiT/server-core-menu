9059d0f2bc696702c943e49c490860f2df5dc1df
Thomas Erdösi <github@erdoesi.de>
2017-06-08 15:00:34 +0200

    Initial Repository Setup
    Initial Setup of Repository

---
+++ ServerCoreMenu/Script/Main.ps1
@@ -0,0 +1,58 @@
+﻿$BasePath       = Split-Path -Parent $PSScriptRoot
+$MenuItemFolder = "MenuItems"
+$MenuItemPath   = Join-Path $BasePath $MenuItemFolder
+$MenuItems      = Get-ChildItem $MenuItemPath | Where { $_.Name -match "^[1-9][0-9]?\) .+" }
+
+$ErrorFlag      = $false
+
+# Collect menu items
+if ($MenuItems.Count -gt 0) {
+    ForEach ($MenuItem In $MenuItems) {
+        $OptionString  = $MenuItem.BaseName.Split(")")[0]
+        $OptionInteger = [int]$OptionString
+        
+        $MenuItem | Add-Member -Name "OptionString"  -Value $OptionString  -MemberType NoteProperty
+        $MenuItem | Add-Member -Name "OptionInteger" -Value $OptionInteger -MemberType NoteProperty
+    }
+
+    $MenuItems = $MenuItems | Sort -Property OptionInteger
+} else {
+    Write-Host "No files matching pattern 'n) ...' or 'nn) ...' found in folder '$MenuItemPath'."
+    $ErrorFlag = $true
+}
+
+if ($ErrorFlag) {
+    Write-Host
+    Write-Host "Errors occured during initialization."
+    Write-Host "[Press enter to exit]"
+    Read-Host
+    return
+}
+
+# Show Menu
+While ($true) {
+    Clear-Host
+
+    Write-Host $env:computername
+    Write-Host "================================================================================"
+    Write-Host
+
+    ForEach ($MenuItem In $MenuItems) {
+        Write-Host $MenuItem.BaseName
+    }
+
+    Write-Host
+    Write-Host
+
+    $OptionString = Read-Host -Prompt "Enter number to select an option"    
+    $MenuItem     = $MenuItems | Where { $_.OptionString -eq $OptionString }
+    
+    if ($MenuItem -ne $null) {
+        if ($MenuItem[0].Extension.ToUpperInvariant() -eq ".PS1") {
+            Invoke-Expression ("& `"" + $MenuItem[0].FullName + "`"")
+        } else {
+            Invoke-Item $MenuItem[0].FullName
+        }
+    }
+}
+
---
+++ ServerCoreMenu/Setup/Install.ps1
@@ -0,0 +1,8 @@
+﻿$BasePath         = Split-Path -Parent $PSScriptRoot
+$ScriptFolder     = "Script"
+$ScriptName       = "Main.ps1"
+$ScriptPath       = Join-Path $BasePath $ScriptFolder
+$ScriptPath       = Join-Path $ScriptPath $ScriptName
+$AutostartCommand = "cmd.exe /c start powershell -noexit -command `"& " + $ScriptPath + "`""
+
+New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\run -Name "ServerCoreMenu" -Value $AutostartCommand -Type string
