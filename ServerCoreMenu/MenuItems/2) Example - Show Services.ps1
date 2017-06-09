bd4499a2e25d352f1c6ae11c8aeeeee0cb1a6574
Thomas Erdösi <github@erdoesi.de>
2017-06-09 13:30:50 +0200

    Example scripts
    Added example scripts

---
+++ ServerCoreMenu/MenuItems/2) Example - Show Services.ps1
@@ -0,0 +1,1 @@
+﻿cmd.exe /c start powershell -command "Get-Service | Out-Host; Write-Host `"Press [Enter] to close this window`"; Read-Host"
---
+++ ServerCoreMenu/MenuItems/3) Example - Show Devices.ps1
@@ -0,0 +1,1 @@
+﻿cmd.exe /c start powershell -command "Get-WmiObject Win32_PNPEntity | Select Caption | Out-Host; Write-Host `"Press [Enter] to close this window`"; Read-Host"
